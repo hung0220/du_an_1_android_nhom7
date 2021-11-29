887596968123b723c69fb697e95d469c272a3c0f
NguyenHuuHung <nguyenhuuhung070220@gmail.com>
2021-11-29 17:16:46 +0700

    Put thong tin len

--- app/src/main/java/com/example/du_an_1_android/SignuUpTabFragment.java
+++ app/src/main/java/com/example/du_an_1_android/SignuUpTabFragment.java
@@ -164,8 +164,13 @@ public class SignuUpTabFragment extends Fragment {
                             sendUserToHome();
 
                             UUID id = UUID.randomUUID();
-                            ThongTinDangNhap thongTinDangNhap = new ThongTinDangNhap(id.toString(),tk,mk);
+                            ThongTinDangNhap thongTinDangNhap = new ThongTinDangNhap();
+                            thongTinDangNhap.setId(id.toString());
+                            thongTinDangNhap.setTenDangNhap(tk);
+                            thongTinDangNhap.setMatKhau(mk);
                             node.child(id.toString()).setValue(thongTinDangNhap);
+
+
 
                             Intent homeIntent = new Intent(getActivity(), MainActivity.class);
 
