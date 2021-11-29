e608649ed84e86fe908b081d0bfee95af2589d8f
Punpun <kendynhok0305@gmail.com>
2021-11-30 02:45:18 +0700

    Hoan chinh

--- app/src/main/java/com/example/du_an_1_android/thongtin_sothich.java
+++ app/src/main/java/com/example/du_an_1_android/thongtin_sothich.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,9 +16,11 @@ import androidx.core.content.ContextCompat;
 
 public class thongtin_sothich extends AppCompatActivity {
     Button tieptuc_sothich;
-    Button btnTheThao,btnVeDem,btnAmNhac,btnDulich,btnCafe,btnDocsach,btnCauca,btnDientu,btnPhim,btnNauan,btnXemay;
+    Button btnTheThao,btnVeDem,btnAmNhac,btnDulich,btnCafe,btnDocsach,btnCauca,btnDientu,btnAmThuc,btnNauan,btnXemay;
+    Button btnTheThao2,btnVeDem2,btnAmNhac2,btnDulich2,btnCafe2,btnDocsach2,btnCauca2,btnDientu2,btnAmThuc2,btnNauan2,btnXemay2;
     ImageView trove_sothich;
     String sothich1,sothich2,sothich3,sothich4,sothich5,sothich6,sothich7,sothich8,sothich9,sothich10,sothich11;
+    TextView txtErroSoThich;
     int dem = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +45,20 @@ public class thongtin_sothich extends AppCompatActivity {
         btnTheThao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    sothich1 = "Thể thao";
-                    btnTheThao.setBackground(ContextCompat.getDrawable(thongtin_sothich.this, R.drawable.boder_onclick_btn));
-                    btnTheThao.setTextColor(Color.parseColor("#F288AF"));
-                    dem++;
+                sothich1 = "Thể thao";
+                btnTheThao.setVisibility(View.INVISIBLE);
+                btnTheThao2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnTheThao2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich1 = "";
+                btnTheThao2.setVisibility(View.INVISIBLE);
+                btnTheThao.setVisibility(View.VISIBLE);
+                dem--;
                 tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
             }
         });
@@ -53,9 +66,19 @@ public class thongtin_sothich extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sothich2 = "Sống về đêm";
-                btnVeDem.setBackground(ContextCompat.getDrawable(thongtin_sothich.this, R.drawable.boder_onclick_btn));
-                btnVeDem.setTextColor(Color.parseColor("#F288AF"));
+                btnVeDem.setVisibility(View.INVISIBLE);
+                btnVeDem2.setVisibility(View.VISIBLE);
                 dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnVeDem2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich2 = "";
+                btnVeDem2.setVisibility(View.INVISIBLE);
+                btnVeDem.setVisibility(View.VISIBLE);
+                dem--;
                 tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
             }
         });
@@ -63,9 +86,19 @@ public class thongtin_sothich extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sothich3 = "Âm nhạc";
-                btnAmNhac.setBackground(ContextCompat.getDrawable(thongtin_sothich.this, R.drawable.boder_onclick_btn));
-                btnAmNhac.setTextColor(Color.parseColor("#F288AF"));
+                btnAmNhac.setVisibility(View.INVISIBLE);
+                btnAmNhac2.setVisibility(View.VISIBLE);
                 dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnAmNhac2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich3 = "";
+                btnAmNhac2.setVisibility(View.INVISIBLE);
+                btnAmNhac.setVisibility(View.VISIBLE);
+                dem--;
                 tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
             }
         });
@@ -73,9 +106,19 @@ public class thongtin_sothich extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sothich4 = "Du lịch";
-                btnDulich.setBackground(ContextCompat.getDrawable(thongtin_sothich.this, R.drawable.boder_onclick_btn));
-                btnDulich.setTextColor(Color.parseColor("#F288AF"));
+                btnDulich.setVisibility(View.INVISIBLE);
+                btnDulich2.setVisibility(View.VISIBLE);
                 dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnDulich2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich4 = "";
+                btnDulich2.setVisibility(View.INVISIBLE);
+                btnDulich.setVisibility(View.VISIBLE);
+                dem--;
                 tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
             }
         });
@@ -83,23 +126,159 @@ public class thongtin_sothich extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 sothich5 = "Cafe";
-                btnCafe.setBackground(ContextCompat.getDrawable(thongtin_sothich.this, R.drawable.boder_onclick_btn));
-                btnCafe.setTextColor(Color.parseColor("#F288AF"));
+                btnCafe.setVisibility(View.INVISIBLE);
+                btnCafe2.setVisibility(View.VISIBLE);
                 dem++;
                 tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
             }
         });
+        btnCafe2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich5 = "";
+                btnCafe2.setVisibility(View.INVISIBLE);
+                btnCafe.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnDocsach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich6 = "Đọc sách";
+                btnDocsach.setVisibility(View.INVISIBLE);
+                btnDocsach2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnDocsach2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich6 = "";
+                btnDocsach2.setVisibility(View.INVISIBLE);
+                btnDocsach.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnCauca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich7 = "Câu cá";
+                btnCauca.setVisibility(View.INVISIBLE);
+                btnCauca2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnCauca2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich7 = "";
+                btnCauca2.setVisibility(View.INVISIBLE);
+                btnCauca.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnDientu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich8 = "Thể thao điện tử";
+                btnDientu.setVisibility(View.INVISIBLE);
+                btnDientu2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnDientu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich8 = "";
+                btnDientu2.setVisibility(View.INVISIBLE);
+                btnDientu.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnAmThuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich9 = "Ẩm thực";
+                btnAmThuc.setVisibility(View.INVISIBLE);
+                btnAmThuc2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnAmThuc2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich9 = "";
+                btnAmThuc2.setVisibility(View.INVISIBLE);
+                btnAmThuc.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnNauan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich10 = "Nấu ăn";
+                btnNauan.setVisibility(View.INVISIBLE);
+                btnNauan2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnNauan2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich10 = "";
+                btnNauan2.setVisibility(View.INVISIBLE);
+                btnNauan.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnXemay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich11 = "Xe máy";
+                btnXemay.setVisibility(View.INVISIBLE);
+                btnXemay2.setVisibility(View.VISIBLE);
+                dem++;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+        btnXemay2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sothich11 = "";
+                btnXemay2.setVisibility(View.INVISIBLE);
+                btnXemay.setVisibility(View.VISIBLE);
+                dem--;
+                tieptuc_sothich.setText("Tiếp tục "+dem+"/3");
+            }
+        });
+
 
         tieptuc_sothich = findViewById(R.id.btnTT_sothich);
         tieptuc_sothich.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(dem<=3){
-
+                if(dem>3){
+                    txtErroSoThich.setVisibility(View.VISIBLE);
                 }else{
-                    Intent intent = new Intent(thongtin_sothich.this, com.example.du_an_1_android.thongtin_mota.class);
-                    startActivity(intent);
-                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    txtErroSoThich.setVisibility(View.INVISIBLE);
+//                    Intent intent = new Intent(thongtin_sothich.this, com.example.du_an_1_android.thongtin_mota.class);
+//                    startActivity(intent);
+//                    overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                    Toast.makeText(thongtin_sothich.this, ""+sothich3, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(thongtin_sothich.this, ""+sothich5, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(thongtin_sothich.this, ""+sothich7, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(thongtin_sothich.this, ""+sothich1, Toast.LENGTH_SHORT).show();
                 }
 
             }
@@ -118,8 +297,22 @@ public class thongtin_sothich extends AppCompatActivity {
         btnDocsach = findViewById(R.id.btnDocsach);
         btnCauca = findViewById(R.id.btnCauca);
         btnDientu = findViewById(R.id.btnDientu);
-        btnPhim = findViewById(R.id.btnPhim);
+        btnAmThuc = findViewById(R.id.btnAmtThuc);
         btnNauan = findViewById(R.id.btnNauan);
         btnXemay = findViewById(R.id.btnXemay);
+
+        btnTheThao2 = findViewById(R.id.btnTheThao2);
+        btnVeDem2 = findViewById(R.id.btnVeDem2);
+        btnAmNhac2 = findViewById(R.id.btnAmNhac2);
+        btnDulich2 = findViewById(R.id.btnDulich2);
+        btnCafe2 = findViewById(R.id.btnCafe2);
+        btnDocsach2 = findViewById(R.id.btnDocsach2);
+        btnCauca2 = findViewById(R.id.btnCauca2);
+        btnDientu2 = findViewById(R.id.btnDientu2);
+        btnAmThuc2 = findViewById(R.id.btnAmtThuc2);
+        btnNauan2 = findViewById(R.id.btnNauan2);
+        btnXemay2 = findViewById(R.id.btnXemay2);
+
+        txtErroSoThich = findViewById(R.id.txtErroSoThich);
     }
 }
