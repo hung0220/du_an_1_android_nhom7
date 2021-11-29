cb7e4de89c867fe56cdf3b480806b75f2353ae5c
NguyenHuuHung <nguyenhuuhung070220@gmail.com>
2021-11-29 17:16:46 +0700

    Put thong tin len

--- app/src/main/java/com/example/du_an_1_android/thongtin_Name.java
+++ app/src/main/java/com/example/du_an_1_android/thongtin_Name.java
@@ -9,19 +9,37 @@ import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class thongtin_Name extends AppCompatActivity {
     Button tieptuc_name;
     ImageView trove_name;
+    FirebaseDatabase db = FirebaseDatabase.getInstance("https://du-an-1-android-75d60-default-rtdb.firebaseio.com/");
+    DatabaseReference node = db.getReference("TaiKhoan");
+    FirebaseAuth firebaseAuth;
+    TextInputEditText edtTenCuaBan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_thongtin_name);
+        firebaseAuth=FirebaseAuth.getInstance();
         //         tiếp tục
         tieptuc_name = findViewById(R.id.btnTT_ten);
         tieptuc_name.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String id = firebaseAuth.getUid();
+                edtTenCuaBan = findViewById(R.id.edtName);
+                String TenCuaBan = edtTenCuaBan.getText().toString().trim();
+                ThongTinDangNhap thongTinDangNhap = new ThongTinDangNhap();
+                thongTinDangNhap.setTenCuaBan(TenCuaBan);
+
+                node.child(id).updateChildren(thongTinDangNhap.toMapTenCuaBan());
+
                 Intent intent = new Intent(thongtin_Name.this, com.example.du_an_1_android.thongtin_ngaysinh.class);
                 startActivity(intent);
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
